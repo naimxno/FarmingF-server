adba3553a96ed906d7ba78ee1448f41853bd7cc5
naimxno <naimxno@gmail.com>
2022-05-27 01:56:19 +0600

    product get

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5ent.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("farming").collection("product");
+
+    app.get('/products', async (req, res) => {
+      const query = {}
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products)
+    });
+
+    app.get('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const orderProduct = await productCollection.findOne(query);
+      res.send(orderProduct)
+    });
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
